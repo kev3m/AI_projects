e69040c8f1077a3df8613d8a3c805223eafd0346
Keven C <keven_jkc@hotmail.com>
2022-10-15 01:06:53 -0300

    validando e treinando o modelo

--- movieRatingAI.py
+++ movieRatingAI.py
@@ -55,6 +55,7 @@ vocab_size = 10000
 
 model = keras.Sequential()
 model.add(keras.layers.Embedding(vocab_size,16))
+model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation='relu'))
 model.add(keras.layers.Dense(1, activation='sigmoid'))
 
@@ -63,4 +64,14 @@ print(model.summary())
 #Usando função Loss para tratar a saída em probabilidade (deve ser em binário)
 model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 
-#----------- Conjunto de validação --------------------------------
+#----------- Conjunto de validação e treinamento do modelo --------------------------------
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
+
+history = model.fit(partial_x_train,partial_y_train,epochs=40,batch_size=512,validation_data=(x_val, y_val),verbose=1)
+
+results = model.evaluate(test_data, test_labels, verbose=2)
+print(results)
