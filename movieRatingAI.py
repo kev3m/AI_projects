f017c8346561c09765610d769872b055e135b5f2
Keven C <keven_jkc@hotmail.com>
2022-10-14 22:57:46 -0300

    preparando dados

--- movieRatingAI.py
+++ movieRatingAI.py
@@ -10,16 +10,17 @@ def decode_review(text):
 #Baixando base de dados imdb
 imdb = keras.datasets.imdb
 
-(train_data,train_labels), (test_data,test_labels) = imdb.load_data(num_words=10000) #num_words=10000 mantém as 10000 palavras mais frequentes.
+#num_words=10000 mantém as 10000 palavras mais frequentes.
+(train_data,train_labels), (test_data,test_labels) = imdb.load_data(num_words=10000) 
  
 print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
 # print(train_data[0]) #Palavras na avaliação [0] -> Cada inteiro representa uma palavra.
+
 print(len(train_data[0])) #Número de palavras na avaliação [0]
 
 
 #---------- Convertendo inteiros em palavras ----------------
 word_dict = imdb.get_word_index()
-
 
 # for movie in word_dict.items():
 #     print(movie[0])
@@ -33,3 +34,15 @@ word_dict["<UNUSED>"] = 3
 reverse_word_dict = dict([(value, key) for (key, value) in word_dict.items()])
 
 print(decode_review(train_data[0]))
+
+#---------- Preparando os dados | Convertendo arrays em tensores --------------------------------
+
+
+#Tensores (estrutura semelhante a tupla, que contém o mesmo tipo de dado)
+
+#pad_sequencer é utilizado para padronizar os tamanhos
+train_data = keras.preprocessing.sequence.pad_sequences(train_data,value=word_dict["<PAD>"], padding='post', maxlen=256)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data,value=word_dict["<PAD>"],padding='post', maxlen=256)
+
+# print(len(train_data[0]), len(train_data[1])) -> Tamanho dos exemplos
+print(train_data[0])
